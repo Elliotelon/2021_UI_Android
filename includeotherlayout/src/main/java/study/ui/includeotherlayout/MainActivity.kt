2811640aca7ba52e8a668935bfd36ad79f15044d
kimminkyu <kmk50009@naver.com>
2021-06-27 20:29:18 +0900

    UI IncludeOtherLayout

---
+++ includeotherlayout/src/main/java/study/ui/includeotherlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package study.ui.includeotherlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
